6b7ed2048edb0fec177734000856f67519c0ce95
djon125 <ross.dion9027@gmail.com>
2022-06-02 17:52:59 -0500

    Added instructions for finishing touches and practice

--- main.py
+++ main.py
@@ -142,3 +142,23 @@ transport_fin = final_transport_picker(random_transport)
 entertainment_fin = final_entertainment_picker(random_entertainment)
 # I put all 4 funciton calls down here and build a big print statment to display the entire trip to the user!
 print(f'You will enjoy a trip to {location_fin} where you will love a {restaurant_fin} meal, get there by {transport_fin} and be entertained by {entertainment_fin}! Have fun!')
+
+# (5 points): As a developer, I want to make at least three commits with descriptive messages.
+
+# (5 points): As a developer, I want to store my destinations, restaurants, mode of transportations, and entertainments in their own separate lists.
+
+# (5 points): As a user, I want a destination to be randomly selected for my day trip.
+
+# (5 points): As a user, I want a restaurant to be randomly selected for my day trip.
+
+# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
+
+# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+
+# (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
+
+# (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
+
+# (10 points): As a user, I want to display my completed trip in the console.
+
+# (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
